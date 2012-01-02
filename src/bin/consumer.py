a7f7ed499b15847aeb8a36b8cc4ecfcdb279b148
Juliano Martinez <juliano@falaserio.org>
2012-01-02 20:38:03 -0200

    working release :D

--- src/bin/consumer.py
+++ src/bin/consumer.py
@@ -6,7 +6,7 @@ from kombu.utils.debug import setup_logging
 
 setup_logging(loglevel="INFO")
 
-config_file = '../etc/logix.conf'
+config_file = '/etc/logix/logix.conf' if os.path.isfile('/etc/logix/logix.conf') else os.getenv('LOGIX_CONF')
 if not os.path.isfile(config_file):
     print "Config file %s not found" % config_file
     sys.exit(1)
@@ -15,8 +15,7 @@ config = ConfigParser.RawConfigParser()
 config.read(config_file)
 
 with BrokerConnection(config.get('transport','url')) as conn:
-    with conn.SimpleQueue(config.get('transport', 'queue'), serializer="json",
-            compression="zlib") as queue:
+    with conn.SimpleQueue(config.get('transport', 'queue'), serializer="json", as queue:
         message = queue.get(block=True, timeout=10)
         if message:
             print message.payload
--- src/bin/logix.py
+++ src/bin/logix.py
@@ -3,17 +3,22 @@
 # forked from Juliano Martinez (http://github.com/ncode/logix)
 # gevent based syslog/amqp(via kombu) bridge
 
-import os, re, sys, math, time, socket, ConfigParser
-import logging
-from kombu.common import maybe_declare
-from kombu import BrokerConnection
-from kombu.pools import producers, connections
-from kombu import Exchange, Queue
-
+import os 
+import re 
+import sys
+import math
+import time 
 import gevent
+import logging
+import ConfigParser
+from kombu import Exchange, Queue
+from kombu import BrokerConnection
+from kombu.common import maybe_declare
+from kombu.pools import producers, connections
 from gevent import socket, monkey, baseserver
 monkey.patch_all()
 
+hostname = os.uname()[1]
 severity = ['emerg', 'alert', 'crit', 'err', 'warn', 'notice', 'info', 'debug']
 facility = ['kern', 'user', 'mail', 'daemon', 'auth', 'syslog', 'lpr', 'news', 'uucp', 
             'cron', 'authpriv', 'ftp', 'ntp', 'audit', 'alert', 'at', 'local0', 'local1', 
@@ -23,18 +28,17 @@ fs_match = re.compile("<(.+)>(.*)", re.I)
 
 def parse_and_queue_datagram(line, host, queue, connection):
     parsed = {}
-    parsed['line'] = line.strip()
-    (fac, sev) = _calc_lvl(parsed['line'])
-    parsed['host'] = host
+    (fac, sev, parsed['line']) = _calc_lvl(line.strip())
+    parsed['host'] = host if not '127.0.0.1' in host else hostname
     parsed['timestamp'] = time.time()
     parsed['facility'] = fac
-    parsed['severity'] = sev
+    parsed['level'] = sev
     parsed['version'] = '1.0'
-    parsed['short_message'] = "<%s> %s %s" % (sev, os.uname()[1], parsed['line'])
+    parsed['short_message'] = "%s" % (parsed['line'])
 
     with connections[connection].acquire(block=True) as conn:
         with conn.SimpleQueue(queue) as queue:
-            queue.put(parsed, serializer="json", compression="zlib")
+            queue.put(parsed, serializer="json")
 
 def _calc_lvl(line):
     lvl = fs_match.split(line)
@@ -42,13 +46,13 @@ def _calc_lvl(line):
         i = int(lvl[1])
         fac = int(math.floor(i / 8))
         sev = i - (fac * 8)
-        return (facility[fac], severity[sev])
-    return (None, None)
+        return (facility[fac].capitalize(), sev, lvl[2])
+    return (None, None, lvl[2])
 
 if __name__ == '__main__':
     logging.info("Starting Logix")
 
-    config_file = '../etc/logix.conf'
+    config_file = '/etc/logix/logix.conf' if os.path.isfile('/etc/logix/logix.conf') else os.getenv('LOGIX_CONF')
     if not os.path.isfile(config_file):
         logging.error("Config file %s not found" % config_file)
         sys.exit(1)
@@ -58,6 +62,7 @@ if __name__ == '__main__':
 
     # config.getint('transport', 'connection_pool_size')
     connection = BrokerConnection(config.get('transport','url')) 
+    connection.connect()
 
     s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
     s.bind(('',config.getint('server', 'port')))
