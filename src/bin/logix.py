d0070569342654ada70fc5fea04202ef087b930d
Gleicon Moraes <gleicon@gmail.com>
2012-01-02 16:23:49 -0200

    rewrite using simplequeue

--- src/bin/logix.py
+++ src/bin/logix.py
@@ -3,11 +3,11 @@
 # forked from Juliano Martinez (http://github.com/ncode/logix)
 # gevent based syslog/amqp(via kombu) bridge
 
-import os, re, sys, math, time, socket, ConfigParser, json
+import os, re, sys, math, time, socket, ConfigParser
 import logging
 from kombu.common import maybe_declare
 from kombu import BrokerConnection
-from kombu.pools import producers
+from kombu.pools import producers, connections
 from kombu import Exchange, Queue
 
 import gevent
@@ -21,7 +21,7 @@ facility = ['kern', 'user', 'mail', 'daemon', 'auth', 'syslog', 'lpr', 'news', '
 
 fs_match = re.compile("<(.+)>(.*)", re.I)
 
-def parse_and_queue_datagram(line, host, queue):
+def parse_and_queue_datagram(line, host, queue, connection):
     parsed = {}
     parsed['line'] = line.strip()
     (fac, sev) = _calc_lvl(parsed['line'])
@@ -31,10 +31,10 @@ def parse_and_queue_datagram(line, host, queue):
     parsed['severity'] = sev
     parsed['version'] = '1.0'
     parsed['short_message'] = "<%s> %s %s" % (sev, os.uname()[1], parsed['line'])
-    payload = json.dumps(parsed)
-    with producers[connection].acquire(block=True) as producer:
-        maybe_declare(Exchange(queue, type="direct"), producer.channel)
-        producer.publish(payload, routing_key="")
+
+    with connections[connection].acquire(block=True) as conn:
+        with conn.SimpleQueue(queue) as queue:
+            queue.put(parsed, serializer="json", compression="zlib")
 
 def _calc_lvl(line):
     lvl = fs_match.split(line)
@@ -56,8 +56,8 @@ if __name__ == '__main__':
     config = ConfigParser.RawConfigParser()
     config.read(config_file)
 
-    for a in xrange(config.getint('amqp', 'connection_pool_size')):
-        connection = BrokerConnection(config.get('amqp','url'))
+    # config.getint('amqp', 'connection_pool_size')
+    connection = BrokerConnection(config.get('amqp','url')) 
 
     s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
     s.bind(('',config.getint('server', 'port')))
@@ -65,5 +65,6 @@ if __name__ == '__main__':
     while True:
         data, peer = s.recvfrom(config.getint('server', 'max_syslog_line_size'))
         logging.debug("New data: %s" % data)
-        gevent.spawn(parse_and_queue_datagram, data, peer, config.get('amqp', 'queue'))
+        gevent.spawn(parse_and_queue_datagram, data, peer, config.get('amqp',
+            'queue'), connection)
 
